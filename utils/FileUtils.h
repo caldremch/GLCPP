0a14c017e86c1c23700d9a32d634982e43ea3719
Caldremch <caldremch@CaldremchMBP.local>
2019-03-29 16:32:42 +0800

    add fileutils

--- utils/FileUtils.h
+++ utils/FileUtils.h
@@ -15,11 +15,13 @@ namespace FileSystem {
 //#ifdef MYMAC
 //            const char *glslDir = "glsldata/";
 //#else
- //           const char *glslDir = "glsldata\\";
+        //           const char *glslDir = "glsldata\\";
 //#endif
 
 
-            const char *glslDir = "glsldata";
+//            const char *glslDir = "glsldata";
+        std::string glslStr = "glsldata";
+
 
 
 
@@ -30,41 +32,44 @@ namespace FileSystem {
 
         //std::cout << "length = " << length << std::endl;
 
-            char sep = boost::filesystem::path::preferred_separator;
+//        char p[80] = "\0";
 
-        char p[80] = "\0";
-
-        strcat(p, glslDir);
-        strcat(p, &sep);
-        strcat(p, glslFileName);
+//        strcat(p, glslDir);
+//        strcat(p, glslFileName);
 
         //std::cout << "p = " << p << std::endl;
 
         boost::filesystem::path execPath = boost::filesystem::current_path();
 
+        std::string glslDataRelativePath = glslStr + boost::filesystem::path::preferred_separator + glslFileName;
+
         std::string rootPath = execPath.parent_path().string();
 
-        std::string absPath = boost::filesystem::complete(p, execPath.parent_path()).string();
+        std::string absPath = boost::filesystem::complete(glslDataRelativePath, execPath.parent_path()).string();
 
         return absPath;
     }
 
 }
-
+//
 //int main() {
 //
-//    const char *p1 = FileSystem::getPath("camera_fragment1.glsl");
-//    const char *p2 = FileSystem::getPath("camera_fragment2.glsl");
+//    std::string p1 = FileSystem::getPath("camera_fragment1.glsl");
+//    std::string p2 = FileSystem::getPath("camera_fragment2.glsl");
 //
 //
+//    std::string str1 = "well";
+//    std::string str2 = "donw";
+//    char sep = boost::filesystem::path::preferred_separator;
+//
+//    std::string str3 = str1+str2+sep;
 //
 //    std::cout << "path1 address:" << p1 << std::endl;
 //    std::cout << "path2 address:" << p2 << std::endl;
 //
 //    std::cout << "path1 :" << &p1 << std::endl;
 //    std::cout << "path2 :" << &p2 << std::endl;
-//
-//
+//    std::cout << "str3 :" << str3<< std::endl;
 //
 //    return 0;
 //}
